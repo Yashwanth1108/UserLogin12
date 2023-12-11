6cfb05ba8cfca85f8dccd12de41be6b0825957ad
yash <yash110820@gmail.com>
2023-12-11 16:59:01 +0530

    added login condition

--- script.js
+++ script.js
@@ -20,36 +20,79 @@ signUpBtn.addEventListener("click", () => {
   signInBtn.classList.add("disable");
 });
 
-signUpBtn.addEventListener("click", signUp);
-// Add the login functionality
-signInBtn.addEventListener("click", login);
-
-function signUp() {
-  // Get user data
-  const name = nameInput.value;
-  const email = emailInput.value;
-  const password = passwordInput.value;
-
-  // Store user data in localStorage (you might want a more secure solution)
-  localStorage.setItem("user", JSON.stringify({ name, email, password }));
-
-  // Redirect to the welcome page
-
-  // Prevent form submission (you can remove this if you want the form to submit)
-  return false;
-}
+const users = [
+  { name: "yash", email: abcgmail.com, password: "pass1", grade: "A" },
+  { name: "user2", email: abcgmail.com, password: "pass2", grade: "B" },
+  { name: "user3", email: abcgmail.com, password: "pass3", grade: "C" },
+];
 
 function login() {
-  let userData = localStorage.getItem("user");
+  const usernameInput = document.getElementById("name").value;
+  const passwordInput = document.getElementById("password").value;
 
-  const { name, email, password } = userData;
-  if (userData) {
-    console.log("Login sucessfull.");
-    window.location.href = "home.html";
+  const user = users.find(
+    (u) => u.username === usernameInput && u.password === passwordInput
+  );
+
+  if (user) {
+    showProfile(user);
+  } else {
+    alert("Invalid username or password");
   }
-
-  // Implement login functionality here
-  // You can check the entered email and password against stored data
-  // and redirect to the welcome page if credentials are valid
-  // For simplicity, let's just log a message for now
 }
+
+function showProfile(user) {
+  const loginContainer = document.getElementById("input-group");
+  const profileContainer = document.getElementById("profile-container");
+  const profileUsername = document.getElementById("profile-username");
+  const profileGrade = document.getElementById("profile-grade");
+
+  loginContainer.style.display = "none";
+  profileContainer.style.display = "block";
+
+  profileUsername.textContent = "Username: " + user.username;
+  profileGrade.textContent = "Grade: " + user.grade;
+}
+
+function showExamResult() {
+  const user = users.find(
+    (u) =>
+      u.username ===
+      document.getElementById("profile-username").textContent.split(": ")[1]
+  );
+  alert("Exam Result: Your grade is " + user.grade);
+}
+
+// signUpBtn.addEventListener("click", signUp);
+// // Add the login functionality
+// signInBtn.addEventListener("click", login);
+
+// function signUp() {
+//   // Get user data
+//   const name = nameInput.value;
+//   const email = emailInput.value;
+//   const password = passwordInput.value;
+
+//   // Store user data in localStorage (you might want a more secure solution)
+//   localStorage.setItem("user", JSON.stringify({ name, email, password }));
+
+//   // Redirect to the welcome page
+
+//   // Prevent form submission (you can remove this if you want the form to submit)
+//   return false;
+// }
+
+// function login() {
+//   let userData = localStorage.getItem("user");
+
+//   const { name, email, password } = userData;
+//   if (userData) {
+//     console.log("Login sucessfull.");
+//     window.location.href = "home.html";
+//   }
+
+//   // Implement login functionality here
+//   // You can check the entered email and password against stored data
+//   // and redirect to the welcome page if credentials are valid
+//   // For simplicity, let's just log a message for now
+// }
